feaf7746c9239dae0b9de4b2e371c79582188df0
Muhammad Faizan Khurshid <faizankhurshid23@gmail.com>
2018-11-26 01:38:52 +0500

    Removed extra code from App.js

--- App.js
+++ App.js
@@ -18,8 +18,6 @@ export default class App extends React.Component {
   render() {
       return (
         <View style={{flex: 1}}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
         <Navigator />
       </View>
       )
