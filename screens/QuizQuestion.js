cb58c97d061a00e14c341aeb77b7a6ca08eabbcf
Muhammad Faizan Khurshid <faizankhurshid23@gmail.com>
2018-11-27 02:14:30 +0500

    included options with questions

--- screens/QuizQuestion.js
+++ screens/QuizQuestion.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableOpacity  } from 'react-native';
-
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+var radio_props = [
+    {label: 'param1', value: 0 },
+    {label: 'param2', value: 1 }
+  ];
+   
 export default class QuizQuestion extends React.Component{
     constructor(props){
         super(props)
@@ -28,13 +33,35 @@ export default class QuizQuestion extends React.Component{
 
     }
 
+    radioPropsFunc(){
+        const { data } = this.state
+        const a = []
+        data[questionNo].incorrect_answers.map((value, index) => {
+            a.push({ label : value, value : index })
+       })
+       return  a
+    }
+    
+
         render(){
             const { data, questionNo } = this.state
+            data && console.log( "s" )
+            
             return(
                 <View style={{flex : 1, alignItems : "center", justifyContent : "center"}}>
                     { data !== undefined ? 
                         <View>
                             <Text>{data[questionNo].question}</Text>
+                            <RadioForm
+                                radio_props={[
+                                    {label : data[questionNo].incorrect_answers[0], value : 0},
+                                    {label : data[questionNo].incorrect_answers[1], value : 1 },
+                                    {label : data[questionNo].incorrect_answers[2], value : 3},
+                                    {label : data[questionNo].correct_answer, value : 4}
+                                ]}
+                                initial={0}
+                                onPress={(value) => {this.setState({value:value})}}
+                                />
                             <Button 
                                 onPress = {() => this.nextQuestion()}
                                 title = "Next Question"
